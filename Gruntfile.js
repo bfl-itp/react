28ac374fe1c3b0bacf67cf841d7c295e521429f0
Aidan Feldman <aidan.feldman@gmail.com>
2014-09-11 04:05:07 -0400

    expand jshint options

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,25 @@ module.exports = function(grunt) {
     jshint: {
       files: {
         src: [
+          '**/.html',
           '**/*.js',
-          '!node_modules/**/*'
+          // excluded files
+          '!node_modules/**/*',
+          '!vendor/**/*'
         ]
+      },
+      // http://www.jshint.com/docs/options/
+      options: {
+        browser: true,
+        eqeqeq: true,
+        extract: 'auto', // http://bahmutov.calepin.co/linting-javascript-inside-html.html
+        forin: true,
+        freeze: true,
+        latedef: true,
+        maxdepth: 4,
+        maxparams: 3,
+        maxstatements: 10,
+        newcap: true
       }
     },
     validation: {
